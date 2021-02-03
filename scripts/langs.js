4024cd483d863811d93b3273e47084c59c2434a7
Damon Du <604821528@qq.com>
2021-02-03 10:42:42 +0800

    test action

--- scripts/langs.js
+++ scripts/langs.js
@@ -4,15 +4,16 @@ module.exports=function(){
       "name": "Simplified Chinese",
       "code": "zh-hans",
       "reviewers": ["guguji5"]
-    },{
-      "name": "Portuguese",
-      "code": "pt-pt",
-      "reviewers": ["mstrlaw"]
-    },{
-      "name": "Spanish",
-      "code": "es",
-      "reviewers": ["codesandtags","crorodriguezro","diegohoyos"]
-    },
+    }
+    // ,{
+    //   "name": "Portuguese",
+    //   "code": "pt-pt",
+    //   "reviewers": ["mstrlaw"]
+    // },{
+    //   "name": "Spanish",
+    //   "code": "es",
+    //   "reviewers": ["codesandtags","crorodriguezro","diegohoyos"]
+    // },
     
   ]
 }
