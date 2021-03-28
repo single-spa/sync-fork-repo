d44ca1bd2aae4c6fcf6a6b88dc95919613d62a2f
Damon <604821528@qq.com>
2021-03-28 08:59:40 +0800

    Update langs.js

--- scripts/langs.js
+++ scripts/langs.js
@@ -1,11 +1,11 @@
 module.exports=function(){
   return [
     {
-      "name": "Simplified Chinese",
-      "code": "zh-hans",
-      "reviewers": ["guguji5"]
-    }
-    ,{
+//       "name": "Simplified Chinese",
+//       "code": "zh-hans",
+//       "reviewers": ["guguji5"]
+//     }
+//     ,{
       "name": "Portuguese",
       "code": "pt-pt",
       "reviewers": ["mstrlaw"]
