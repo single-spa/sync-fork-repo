45a389b5d002515a1bb6d68fdc1df6f509dde640
guguji5 <604821528@qq.com>
2022-01-22 13:21:29 +0800

    feat: test github action

--- scripts/langs.js
+++ scripts/langs.js
@@ -1,23 +1,24 @@
-module.exports=function(){
+module.exports = function () {
   return [
     {
-//       "name": "Simplified Chinese",
-//       "code": "zh-hans",
-//       "reviewers": ["guguji5"]
-//     }
-//     ,{
-      "name": "Portuguese",
-      "code": "pt-pt",
-      "reviewers": ["mstrlaw"]
-    },{
-      "name": "Spanish",
-      "code": "es",
-      "reviewers": ["codesandtags"]
-    },{
-      "name": "Russian",
-      "code": "ru",
-      "reviewers": ["Smekalin","astra1"]
+      name: "Simplified Chinese",
+      code: "zh-hans",
+      reviewers: ["guguji5"],
     },
-    
-  ]
-}
+    {
+      name: "Portuguese",
+      code: "pt-pt",
+      reviewers: ["mstrlaw"],
+    },
+    {
+      name: "Spanish",
+      code: "es",
+      reviewers: ["codesandtags"],
+    },
+    {
+      name: "Russian",
+      code: "ru",
+      reviewers: ["Smekalin", "astra1"],
+    },
+  ];
+};
